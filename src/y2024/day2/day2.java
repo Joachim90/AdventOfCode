d2bd0d80c2a5d1397f87a323a853462c57430a15
Joachim Wiltman <jke@live.se>
2025-02-17 13:06:40 +0100

    Another solved

---
+++ src/y2024/day2/day2.java
@@ -0,0 +1,53 @@
+package y2024.day2;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+public class day2 {
+    public static void main(String[] args) throws IOException {
+        List<String> input = Files.lines(Path.of("src/y2024/day2/inputday2")).toList();
+
+        int safe = 0;
+
+
+        for (int i = 0; i < input.size(); i++) {
+            int safeSteps = 0;
+            String line = input.get(i);
+            String[] array = (line.split(" "));
+
+            for (int j = 0; j < array.length - 1; j++) {
+                if (Integer.parseInt(array[j]) > Integer.parseInt(array[j+1]) && Integer.parseInt(array[j]) - Integer.parseInt(array[j+1]) <= 3) {
+                    safeSteps++;
+                }
+            }
+            if (safeSteps == array.length -1) {
+                safe++;
+            }
+        }
+
+
+        for (int i = 0; i < input.size(); i++) {
+            int safeSteps = 0;
+            String line = input.get(i);
+            String[] array = (line.split(" "));
+
+            for (int j = 0; j < array.length - 1; j++) {
+                if (Integer.parseInt(array[j]) < Integer.parseInt(array[j+1]) && Integer.parseInt(array[j+1]) - Integer.parseInt(array[j]) <= 3){
+                    safeSteps++;
+                }
+            }
+            if (safeSteps == array.length -1){
+                safe++;
+            }
+        }
+
+        System.out.println("Svar: " + safe);
+
+
+    }
+}
